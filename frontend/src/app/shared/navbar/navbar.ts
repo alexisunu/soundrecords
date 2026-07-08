8aae3b8db4156813d6cfdf0f2deef81fd584fede
Aspy2005 <lalejandro.2005martinez@gmail.com>
2026-07-08 14:46:22 -0500

    Navbar aplicado

--- frontend/src/app/shared/navbar/navbar.ts
+++ frontend/src/app/shared/navbar/navbar.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../core/services/auth';
+import { User } from '../../core/models/user.model';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  user$: Observable<User | null>;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.user$ = this.authService.currentUser$;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
