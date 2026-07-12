b63093a5fe852b5d7e591ff4e738bc62fd11fb31
alexisunu <wilforalexis07@gmail.com>
2026-07-12 15:07:11 -0500

    Implement review update and delete functionality; add CORS configuration and JWT interceptor logging

--- frontend/src/app/core/interceptors/jwt-interceptor.ts
+++ frontend/src/app/core/interceptors/jwt-interceptor.ts
@@ -1,17 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { AuthService } from '../services/auth'; // <- antes: '../services/auth.service'
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(AuthService);
-  const token = authService.getToken();
+  // Ajusta 'token' al nombre exacto de la llave donde guardas tu JWT
+  const token = localStorage.getItem('sr_token'); 
+  
+  console.log('--- INTERCEPTOR EJECUTÁNDOSE ---');
+  console.log('URL de la petición:', req.url);
+  console.log('Token encontrado en el navegador:', token);
 
   if (token && req.url.includes('/api/')) {
+    console.log('¡Condición cumplida! Adjuntando token...');
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
+  } else {
+    console.log('No se adjuntó el token. ¿Falta el token o la URL no incluye /api/?');
   }
 
   return next(req);
